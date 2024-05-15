28ee664c5332839db9f5613500a3d47d1af0d7b6
zaelanirizal02 <zaelanirizal02@gmail.com>
2024-05-15 23:40:12 +0700

    dashboard

---
+++ frontend/src/views/dashboard/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <masterChart />
+</template>
+<script>
+
+import '../../assets/css/buttonStyle.css';
+import '../employes/style.css';
+
+import masterChart from '../../components/masterChart.vue'
+export default {
+    components: {
+        masterChart,
+    }
+}
+</script>
