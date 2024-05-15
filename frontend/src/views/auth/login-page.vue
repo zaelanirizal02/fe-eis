f34eb8a8479166094e23913e30a1cf16cafb8334
zaelanirizal02 <zaelanirizal02@gmail.com>
2024-05-15 23:42:44 +0700

    penambahan halaman login

--- frontend/src/views/auth/login-page.vue
+++ frontend/src/views/auth/login-page.vue
@@ -1,96 +1,70 @@
-<!-- <template>
-    <div class="card">
-        <div class="flex flex-column md:flex-row">
-            <div class="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-3 py-5">
-                <div class="flex flex-wrap justify-content-center align-items-center gap-2">
-                    <label class="w-6rem">Username</label>
-                    <InputText id="username" type="text" class="w-12rem" />
-                </div>
-                <div class="flex flex-wrap justify-content-center align-items-center gap-2">
-                    <label class="w-6rem">Password</label>
-                    <InputText id="password" type="password" class="w-12rem" />
-                </div>
-                <Button label="Login" icon="pi pi-user" class="w-10rem mx-auto"></Button>
-            </div>
-            <div class="w-full md:w-2">
-                <Divider layout="vertical" class="hidden md:flex"><b>OR</b></Divider>
-                <Divider layout="horizontal" class="flex md:hidden" align="center"><b>OR</b></Divider>
-            </div>
-            <div class="w-full md:w-5 flex align-items-center justify-content-center py-5">
-                <Button label="Sign Up" icon="pi pi-user-plus" severity="success" class="w-10rem"></Button>
-            </div>
-        </div>
-    </div>
-</template> -->
-
-<!-- <script>
-import './auth.css';
-</script>
-
 <script>
-import api from '../../api';
-import "../../assets/form.css";
+import "../auth/auth.css"
+import { hrisServiceAuthInfo, token } from "../../api/index";
 
 export default {
     data() {
         return {
-            username: "",
-            password: "",
+            username: '',
+            password: ''
         };
     },
     methods: {
-
-
         async login() {
-
             try {
-                const response = await api.post('api/auth/login', {
-                    username: this.username,
-                    password: this.password,
-                },);
-                // console.log(response.data.data.accessToken);
-
+                const response = await hrisServiceAuthInfo.post(
+                    "auth/sign-in/login",
+                    {
+                        namaUser: this.username,
+                        kataSandi: this.password
+                    },
+                    {
+                        headers: {
+                            "x-auth-token": token // Menggunakan token dari import
+                        }
+                    }
+                );
+                // Log respons untuk memeriksa struktur respons
+                console.log(response.data);
                 if (response.status === 200) {
-                    // Login berhasil
-                    const token = response.data.data.accessToken;
-                    // Anda dapat menyimpan token yang diberikan dalam respons di sini
-                    // Simpan token akses di localStorage
+                    const token = response.data.data.token;
+                    // Pastikan nama 'accessToken' sesuai saat menyimpan ke localStorage
                     localStorage.setItem('accessToken', token);
 
-                    this.$router.push({ path: '/' });
-                    // Redirect ke halaman yang sesuai
+                    // Redirect ke halaman "/dashboard" setelah login berhasil
+                    this.$router.push("/dashboard");
                 } else {
                     // Tangani situasi lain jika diperlukan
                     console.log("Gagal login");
                 }
-
             } catch (error) {
                 // Tangani kesalahan jika terjadi
-                console.error("Terjadi kesalahan saat login:", error);
+                console.error('Login error:', error);
             }
         },
-    },
-
-
-};
-</script> -->
-
-<script>
-import "../auth/auth.css"
+        loginOnEnter(event) {
+            if (event.key === 'Enter') {
+                this.login();
+            }
+        }
+    }
+}
 </script>
 
 <template>
-    <div class="container">
-        <div class="card logo">
+    <div class="containerLogin">
+        <div class="cardLogin logo">
             <div class="flex flex-column md:flex-row">
                 <div class="w-full md:w-5 flex flex-column align-items-center justify-content-center gap-3 py-8">
                     <div class="flex flex-wrap justify-content-center align-items-center gap-2">
                         <label class="w-6rem">Username</label>
-                        <InputText v-model="username" id="username" type="text" class="w-12rem" />
+                        <InputText v-model="username" id="username" type="text" class="w-12rem"
+                            @keydown.enter="loginOnEnter" />
                     </div>
                     <div class="flex flex-wrap justify-content-center align-items-center gap-2">
                         <label class="w-6rem">Password</label>
-                        <InputText v-model="password" id="password" type="password" class="w-12rem" />
+                        <InputText v-model="password" id="password" type="password" class="w-12rem"
+                            @keydown.enter="loginOnEnter" />
                     </div>
 
                 </div>
@@ -118,7 +92,7 @@ import "../auth/auth.css"
     height: 300px;
     padding: 15px;
     background-color: rgb(34, 50, 74);
-} */
+} 
 
 /* .center {
     margin: center;
